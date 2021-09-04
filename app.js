4eedbf40f8e62d1f32cf79430bdd5ee525bca7e9
AdamZullowa06 <adamzullowa06@gmail.com>
2021-09-04 21:50:07 +0700

    adding feature post, delete, update contact

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+
+const {body, validationResult, check} = require('express-validator');
+const methodOverride = require('method-override');
+
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
@@ -9,6 +13,8 @@ const Contact = require('./model/contact');
 
 const app = express();
 const port = 3000;
+
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
@@ -71,6 +77,85 @@ app.get('/contact/add', (req, res) => {
         layout: 'layouts/main'
     })
 });
+
+app.post('/contact', [
+    body('nama').custom( async (value) => {
+        const duplikat = await Contact.findOne({nama: value});
+        if(duplikat) {
+            throw new Error('Nama kontak sudah ada!');
+        }
+        return true; 
+    }),
+    check('email', 'email tidak valid').isEmail(), 
+    check('nohp', 'No Hp tidak valid').isMobilePhone('id-ID')
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        res.render('add', {
+            title: 'Tambah kontak', 
+            layout: 'layouts/main',
+            errors: errors.array()
+        })
+    } else {
+        Contact.insertMany(req.body, () => {
+            req.flash('msg', 'Kontak berhasil ditambahkan');
+            res.redirect('/contact');
+        });
+    }
+});
+
+app.delete('/contact', (req,res) => {
+    Contact.deleteOne({ nama: req.body.nama }).then((result) => {
+        req.flash('msg', 'Kontak berhasil dihapus!');
+        res.redirect('/contact');
+    });
+});
+
+app.get('/contact/edit/:nama', async (req, res) => {
+    const contact = await Contact.findOne({nama: req.params.nama});
+
+    res.render('edit', {
+        title: 'Edit Kontak',
+        layout: 'layouts/main',
+        contact
+    })
+});
+
+app.put('/contact', [
+    body('nama').custom( async (value, {req}) => {
+        const duplikat = await Contact.findOne({ nama : value });
+        if(value !== req.body.oldNama && duplikat) {
+            throw new Error('Nama contact sudah digunakan!');
+        }
+        return true;
+    }),
+    check('email', 'Email tidak valid').isEmail(),
+    check('nohp', 'No Hp tidak valid').isMobilePhone('id-ID')
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        res.render('edit', {
+            title: 'Edit Kontak',
+            layout: 'layouts/main',
+            errors: errors.array(),
+            contact: req.body
+        })
+    } else {
+        Contact.updateOne(
+            {_id : req.body._id},
+            {
+                $set: {
+                    nama: req.body.nama,
+                    email: req.body.email,
+                    nohp: req.body.nohp
+                }
+            }
+        ).then((result) => {
+            req.flash('msg', 'Kontak berhasil diubah!');
+            res.redirect('/contact');
+        });
+    }
+})
 
 app.get('/contact/:nama', async (req, res) => {
     const contact = await Contact.findOne({ nama: req.params.nama });
